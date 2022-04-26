b58bb0c8f2384cd9a2d330b5fb663f274fa4e3b0
rfraire <104396835+rfraire@users.noreply.github.com>
2022-04-26 10:37:47 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
-import emoji as ej
 
 st.title('My Mom\'s New Healthy Diner')
 
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberr Oatmeal')
 st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
-print(ej.emojize(':banana:'))
+st.text('Hard-Boiled Free-Range Egg'🍌)
