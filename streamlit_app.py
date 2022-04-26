83895756bfd2acc41957c0885bf8e3cfeeb21938
rfraire <104396835+rfraire@users.noreply.github.com>
2022-04-26 16:23:34 -0400

    Update streamlit_app.py
    sets the table to the chosen fruits.

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,6 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include
-st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
-
-st.dataframe(my_fruit_list)
+fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Banana'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+st.dataframe(fruits_to_show)
