d274c8a77885f41c427efa071e9c61b820067e9e
rfraire <104396835+rfraire@users.noreply.github.com>
2022-04-26 10:18:33 -0400

    Update streamlit_app.py
    Added menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ st.title('My Mom\'s New Healthy Diner')
 
 st.header('Breakfast Menu')
 st.text('Omega 3 & Blueberr Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
