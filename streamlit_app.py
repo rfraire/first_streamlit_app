520ac42caae7e848d82e692ab9101c18b97fcb9c
rfraire <104396835+rfraire@users.noreply.github.com>
2022-04-26 16:06:06 -0400

    Update streamlit_app.py
    Fix syntax error

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,6 @@ st.header('🍌 Build Your Own Fruit Smoothie 🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 # Let's put a pick list here so they can pick the fruit they want to include
-st.multiselect("Pick some fruits:", list(my_fruit_list).index))
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 st.dataframe(my_fruit_list)
